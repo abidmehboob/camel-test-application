261c60e8e8ac6fae741a4ba521655a6a5812f583
abid <abidali8@hotmail.com>
2025-05-06 11:27:09 +0200

    [codeai] Generate unit_tests classes

---
+++ src/test/java/com/example/camel/CamelApplicationGeneratedAiTests.java
@@ -0,0 +1,21 @@
+package com.example.camel;
+
+import org.junit.Before;
+import org.junit.After;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+public class CamelApplicationGeneratedAiTests {
+
+    @Test
+    public void testMain() throws Exception {
+        // GIVEN - Set up the context for testing
+        String[] args = {}; 
+
+        // WHEN - Execute the main method of the application
+        SpringApplication.run(CamelApplication.class, args); 
+
+        // THEN - Assert that the application runs successfully (e.g., check for specific output or state)
+    }
+}
